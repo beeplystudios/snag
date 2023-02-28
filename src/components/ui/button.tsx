b2f70246f98d594015f17c7bbe9bb9f4c509143e
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-02-28 16:10:53 -0500

    add interactivity (grrr) to the buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
   children,
   className,
@@ -6,9 +8,11 @@ const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
   return (
     <button
       {...rest}
-      className={`rounded-lg bg-bg-200 px-3 py-[0.4rem] text-highlight disabled:opacity-50 ${
+      className={clsx(
+        "text-highlight rounded-lg bg-bg-200 px-3 py-[0.4rem] disabled:opacity-50",
+        "active:bg-bg-400 scale-100 transform transition-all hover:bg-bg-300 active:scale-95",
         className ?? ""
-      }`}
+      )}
     >
       {children}
     </button>
