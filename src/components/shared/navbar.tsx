71dc209465ab40d841457b35c24b9430638c714f
SantiagoVira <santiagovira06@gmail.com>
2023-02-27 22:32:14 -0500

    logo

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -3,16 +3,14 @@ import Image from "next/image";
 import Tab from "./tab";
 import Link from "next/link";
 import { mainTabs } from "./tabs";
-import { useRouter } from "next/router";
 
 const Navbar: React.FC = () => {
   const { data: sessionData } = useSession();
-  const router = useRouter();
 
   return (
     <div className="flex h-[50px] w-screen items-center justify-between bg-bg-200 px-20 shadow">
       <Link href="/">
-        <Image src="/White Logo.png" alt="" width={30} height={10} />
+        <Image src="/white-logo.svg" alt="" width={30} height={10} />
       </Link>
       {mainTabs.map((tab, i) => (
         <Tab key={i} {...tab} />
@@ -20,14 +18,7 @@ const Navbar: React.FC = () => {
       <button
         className="text-highlight no-underline transition hover:-translate-y-0.5"
         onClick={
-          sessionData
-            ? () => void signOut()
-            : () =>
-                void signIn("google", {
-                  callbackUrl: `/sign-up?redirect=${encodeURIComponent(
-                    router.asPath
-                  )}`,
-                })
+          sessionData ? () => void signOut() : () => void signIn("google")
         }
       >
         {sessionData ? "Sign out" : "Sign in"}
