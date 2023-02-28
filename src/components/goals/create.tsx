22dc66f3a4d363fe3f31c3f6f657175b95f7d6d4
SantiagoVira <santiagovira06@gmail.com>
2023-02-28 15:29:00 -0500

    frontend is better

--- src/components/goals/create.tsx
+++ src/components/goals/create.tsx
@@ -4,19 +4,24 @@ import { useCallback, useState } from "react";
 import type { z } from "zod";
 import { TsForm } from "../ui/forms/ts-form";
 import Button from "../ui/button";
+import { useSession } from "next-auth/react";
 
 export const CreateGoal: React.FC = () => {
   const createGoal = api.goal.create.useMutation();
   const trpcContext = api.useContext();
   const [open, setOpen] = useState(false);
+  const { data: sessionData } = useSession();
 
   const onSubmit = useCallback(
     async (values: z.infer<typeof createGoalSchema>) => {
       await createGoal.mutateAsync(values);
       await trpcContext.goal.getMine.invalidate();
+      setOpen(false);
     },
     [createGoal, trpcContext]
   );
+
+  if (!sessionData?.user) return <></>;
 
   return (
     <div className="flex w-full items-start gap-4 p-4">
@@ -24,14 +29,14 @@ export const CreateGoal: React.FC = () => {
         <button className="hidden" onClick={() => setOpen(!open)} />
         <div className="group relative h-10 w-10 rounded-full bg-bg-300 transition-all hover:scale-105">
           <div
-            className={`absolute left-1/2 top-1/2 mx-auto h-5 w-[0.3rem] -translate-x-1/2 -translate-y-1/2 bg-slate-300 transition-all ${
-              open ? "h-0" : ""
+            className={`absolute left-1/2 top-1/2 mx-auto  w-[0.3rem] -translate-x-1/2 -translate-y-1/2 bg-slate-300 transition-all ${
+              open ? "h-0" : "h-5"
             }`}
           />
           <div className="absolute left-1/2 top-1/2 mx-auto h-[0.3rem] w-5 -translate-x-1/2 -translate-y-1/2 bg-slate-300" />
         </div>
       </label>
-      {open && (
+      {open ? (
         <TsForm
           schema={createGoalSchema}
           // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -63,6 +68,13 @@ export const CreateGoal: React.FC = () => {
             </Button>
           )}
         />
+      ) : (
+        <div
+          className="flex h-10 w-full items-center gap-4 rounded-xl bg-bg-300 px-4"
+          onClick={() => setOpen(!open)}
+        >
+          <p className="my-auto">Add a new goal...</p>
+        </div>
       )}
     </div>
   );
