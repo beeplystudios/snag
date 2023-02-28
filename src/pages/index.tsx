337619fee7f6c043e5cb2d1c02614245b99a2dd4
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-02-28 16:07:35 -0500

    scaffold landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
 import { CreateGoal } from "@/components/goals/create";
 import Goal from "@/components/goals/goal";
 import Layout from "@/components/shared/layout";
+import Button from "@/components/ui/button";
 import { api } from "@/utils/api";
 import { type NextPage } from "next";
+import { signIn, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  const goalsQuery = api.goal.getMine.useQuery();
+  const { status } = useSession();
+  const goalsQuery = api.goal.getMine.useQuery(undefined, {
+    enabled: status === "authenticated",
+  });
+
+  if (status !== "authenticated")
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        {status === "loading" && <p>Logging you in..</p>}
+        {status === "unauthenticated" && (
+          <div>
+            <p>insert landing page here</p>
+            <Button onClick={() => void signIn("google")}>Sign in</Button>
+          </div>
+        )}
+      </div>
+    );
 
   return (
     <Layout>
