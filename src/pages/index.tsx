a27bdf746417f709a62da95096aad45a99b2493c
SantiagoVira <santiagovira06@gmail.com>
2023-02-28 22:10:36 -0500

    more stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,6 +42,10 @@ const Home: NextPage = () => {
       {goalsQuery.data?.map((goal, i) => (
         <Goal goal={goal} key={i} />
       ))} */}
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <h1 className="mt-20 text-2xl font-[700]">Nothing is Selected!!</h1>
+        <p>Select a goal to see more.</p>
+      </div>
     </DashboardLayout>
   );
 };
