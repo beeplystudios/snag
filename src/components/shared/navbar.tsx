325778b2631ebc3275cc3456f9b09309a2ff25f0
awareermine <aramie.ewen@gmail.com>
2023-03-02 21:39:52 -0500

    navbar styles

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -39,8 +39,8 @@ const Navbar: React.FC = () => {
   ];
 
   return (
-    <div className="flex w-full justify-center py-2 shadow-md">
-      <div className="flex w-full max-w-6xl justify-between p-2">
+    <div className="flex gap-2 flex-row p-2 shadow-md">
+      <div className="mr-auto gap-2 self-center">
         <div className="flex items-center gap-4">
           <Link href="/">
             <Image src="/logo.svg" alt="" width={30} height={10} />
@@ -50,63 +50,62 @@ const Navbar: React.FC = () => {
             <Tab key={i} {...tab} />
           ))}
         </div>
-
-        <div className="flex gap-4">
-          <div className="flex gap-2 rounded-md bg-green-300 py-1 px-2 font-medium">
-            <div className="flex gap-2">
-              <Image src="/Snag coin.svg" width={20} height={10} alt="coins" />
-              <p>{session?.user.points}</p>
-            </div>
-            <p>·</p>
-            <p>{session?.user.streak} day streak</p>
-          </div>
-          <Popover>
-            <PopoverTrigger className="flex items-center gap-2 rounded-md px-2 font-medium transition-colors hover:bg-gray-200 ">
-              <BsInbox />
-              {!isMobile && "Inbox"}
-            </PopoverTrigger>
-            <PopoverContent className="h-[28rem] w-[32rem]">
-              <Inbox />
-            </PopoverContent>
-          </Popover>
-
-          <DropdownMenu>
-            <DropdownMenuTrigger className="flex items-center gap-2">
-              <Avatar>
-                <AvatarImage
-                  src={session?.user.image || ""}
-                  alt={session?.user.name || ""}
-                />
-                <AvatarFallback>
-                  {session?.user.name
-                    ?.split(" ")
-                    .map((word) => word[0])
-                    .join("")
-                    .toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              {!isMobile && (
-                <p className="font-medium text-neutral-800">
-                  {session?.user.name}
-                </p>
-              )}
-              <FaChevronDown />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem asChild>
-                <Link href={`/profile/${session?.user.slug || ""}`}>
-                  My Profile
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                onClick={() => void signOut({ callbackUrl: "/" })}
-              >
-                Sign out
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
       </div>
+
+      <div className="flex items-center bg-gray-100 gap-2 rounded-md py-1 px-2 font-medium">
+        <div className="flex gap-2">
+          <Image src="/Snag coin.svg" width={20} height={10} alt="coins" />
+          <p>{session?.user.points}</p>
+        </div>
+        <p>·</p>
+        <p>{session?.user.streak} day streak</p>
+      </div>
+        
+      <Popover>
+        <PopoverTrigger className="flex items-center gap-2 rounded-md p-2 font-medium transition-colors hover:bg-gray-200 ">
+          <BsInbox />
+          {!isMobile && "Inbox"}
+        </PopoverTrigger>
+        <PopoverContent className="h-[28rem] w-[32rem]">
+          <Inbox />
+        </PopoverContent>
+      </Popover>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage
+              src={session?.user.image || ""}
+              alt={session?.user.name || ""}
+            />
+            <AvatarFallback>
+              {session?.user.name
+                ?.split(" ")
+                .map((word) => word[0])
+                .join("")
+                .toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          {!isMobile && (
+            <p className="font-medium text-neutral-800">
+              {session?.user.name}
+            </p>
+          )}
+          <FaChevronDown />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem asChild>
+            <Link href={`/profile/${session?.user.slug || ""}`}>
+              My Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => void signOut({ callbackUrl: "/" })}
+          >
+            Sign out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
